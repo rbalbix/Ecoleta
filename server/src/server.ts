1442a2e6e72490484d19b88f744df338ce8f4bb6
Ricardo Balbi <rbalbi@gmail.com>
2020-06-01 14:33:14 -0300

    chore(web): add web C.R.A. config
    create-react-app with typescript template

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,11 @@
-import express from 'express';
+import express, { response } from 'express';
 
+
+const app = express();
+
+app.get('/', (request, response)=>{
+  console.log('text0');
+  response.send({message:'Hello Typescript!'});
+});
+
+app.listen(3333);
