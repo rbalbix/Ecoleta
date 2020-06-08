cccc29b2db974abaa19b72840637ef798f523a33
Ricardo Balbi <rbalbi@gmail.com>
2020-06-08 13:09:26 -0300

    build(heroku): config postgres on heroku

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,11 +1,21 @@
 import knex from 'knex';
 import path from 'path';
 
+const env = process.env.NODE_ENV;
+
+//DATABASE_URL
+
+const client = env === 'production' ? 'pg' : 'sqlite3';
+const strConnection =
+  env === 'production'
+    ? 'postgres://eagzqdvbsdvdof:f8ecdd900abe6396a757f7df48e9bf3b526d57e1ffad7d2d9cdbdcac61b0178e@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d76t9ff64rgasn'
+    : {
+        filename: path.resolve(__dirname, 'database.sqlite'),
+      };
+
 const connection = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: path.resolve(__dirname, 'database.sqlite'),
-  },
+  client,
+  connection: strConnection,
   useNullAsDefault: true,
 });
 
