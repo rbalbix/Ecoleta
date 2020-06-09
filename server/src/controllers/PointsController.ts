979496541a8576e23c601e092c5979b919b70817
Ricardo Balbi <rbalbi@gmail.com>
2020-06-09 15:36:14 -0300

    fix(server): point url of heroku

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -20,7 +20,7 @@ class PointsController {
     const serializedPoints = points.map((point) => {
       return {
         ...point,
-        image_url: `http://192.168.0.14:3333/uploads/${point.image}`,
+        image_url: `https://rb-ecoleta.herokuapp.com/uploads/${point.image}`,
       };
     });
 
@@ -38,7 +38,7 @@ class PointsController {
 
     const serializedPoint = {
       ...point,
-      image_url: `http://192.168.0.14:3333/uploads/${point.image}`,
+      image_url: `https://rb-ecoleta.herokuapp.com/uploads/${point.image}`,
     };
 
     const items = await knex('items')
