732af90e32d5c10d0b04a4c63c7a0bdb116bef8a
Ricardo Balbi <rbalbi@gmail.com>
2020-06-09 15:13:12 -0300

    build(heroku): database config url changes

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -10,7 +10,8 @@ const env = process.env.NODE_ENV;
 const client = env === 'production' ? 'pg' : 'sqlite3';
 const strConnection =
   env === 'production'
-    ? 'postgres://eagzqdvbsdvdof:f8ecdd900abe6396a757f7df48e9bf3b526d57e1ffad7d2d9cdbdcac61b0178e@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d76t9ff64rgasn'
+    ? // ? 'postgres://eagzqdvbsdvdof:f8ecdd900abe6396a757f7df48e9bf3b526d57e1ffad7d2d9cdbdcac61b0178e@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d76t9ff64rgasn'
+      'postgres://ppfzgyjqodyowq:c26166dde283ebf5e39e8c1f5f99063bd2dc873ddccd01b81a7f9a99569edaee@ec2-50-19-26-235.compute-1.amazonaws.com:5432/d8qvo9ou8qtcgh'
     : // ? 'postgres://eagzqdvbsdvdof:f8ecdd900abe6396a757f7df48e9bf3b526d57e1ffad7d2d9cdbdcac61b0178e@ec2-50-17-90-177.compute-1.amazonaws.com:5432/d76t9ff64rgasn?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory'
       {
         filename: path.resolve(__dirname, 'database.sqlite'),
